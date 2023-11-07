18b912af4abb2354516b55ebcb1a3b5a1f7f5f35
Marcelo Montalba Pavez <mmontalbapavez@gmail.com>
2023-11-07 17:28:59 -0300

    Sistema ventas en Laravel 8 + PHP 7.3

---
+++ sistema_ventas/app/Models/Producto.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    use HasFactory;
+
+    protected $table = "productos";
+    protected $fillable = [
+        "codigo",
+        "nombre",
+        "descripcion",
+        "precio",
+        "url_imagen",
+        "like",
+        "dislike",
+        "user_id"
+    ];
+}
---
+++ sistema_ventas/database/migrations/2023_11_07_182505_create_producto_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string("codigo", 20)->unique();
+            $table->text("descripcion")->nullable();
+            $table->decimal("precio", 10, 2)->default(0);
+            $table->string("url_imagen", 20)->nullable();
+            $table->integer("like")->default(0);
+            $table->integer("dislike")->default(0); 
+
+            // Relaciones
+            $table ->unsignedBigInteger("user_id");
+            $table ->foreign("user_id")
+                   ->references("id")
+                   ->on("users")
+                   ->onUpdate("cascade")
+                   ->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
